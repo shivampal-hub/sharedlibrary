20ad588cb1daa641b6ae520f161461c84fa16fb2
shivampal-hub <70017082+shivampal-hub@users.noreply.github.com>
2023-02-20 01:09:17 +0530

    Create ansible.groovy

---
+++ vars/ansible.groovy
@@ -0,0 +1,45 @@
+def readPropertyFile() {
+  readProp = read Properties file: 'configFilePath'
+  return readProp
+}
+
+def clone() {
+  git branch: 'master', url: ''
+}
+
+def approval() {
+  def p = readPropertyFile()
+  if ("${p.KEEP_APPROVAL_STAGE}" == "true") {
+    input 'Do you want to execute playbook?'
+  }
+}
+
+def playbookExecution() {
+  ansiblePlaybook credentialsId: 'assignment6-ssh', disableHostKeyChecking: true, installation: 'ansible1', inventory: 'inv', playbook: 'role_jenkins.yml'
+}
+
+def slackSend(String buildResult) {
+  def p = readPropertyFile()
+  if ( buildResult == "Success" ) {
+    slackSend channel: "${p.SLACK_CHANNEL_NAME}",
+      message: "${p.ACTION_MESSAGE}",
+      tokenCredentialId: 'slack'
+  }
+  else if ( buildResult == "FAILURE") {
+    slackSend channel: "${p.SLACK_CHANNEL_NAME}",
+    message: message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
+      tokenCredentialId: 'slack'
+  }
+  else if ( buildResult == "UNSTABLE") {
+    slackSend channel: "${p.SLACK_CHANNEL_NAME}",
+    message: message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
+      tokenCredentialId: 'slack'
+  }
+  else {
+    slackSend channel: "${p.SLACK_CHANNEL_NAME}",
+      message: message: " *${currentBuild.currentResult}:* \n *Job_Name:* '${JOB_NAME}' \n *USER:* '${USER}' \n *Stage_Name:* ${STAGE_NAME} \n *Build_Number:* '${BUILD_NUMBER}' \n *More info at:* '${BUILD_URL}'",
+      tokenCredentialId: 'slack'
+  }
+}
+      
+  
